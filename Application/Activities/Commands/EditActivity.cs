2f32619bf9163e7802d210cf8fb04fc96b6c930b
Ricardo <ric.alv_17@hotmail.com>
2026-01-28 19:44:47 -0500

    End of section 4

---
+++ Application/Activities/Commands/EditActivity.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Domain;
+using MediatR;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Activities.Commands
+{
+    public class EditActivity
+    {
+        public class Command: IRequest
+        {
+            public required Activity Activity { get; set; }
+        }
+
+        public class Handler(AppDbContext context, IMapper mapper) : IRequestHandler<Command>
+        {
+            public async Task Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await context.Activities.FindAsync([request.Activity.Id], cancellationToken);
+
+                if (activity == null) throw new Exception("Cannot find activity");
+
+                mapper.Map(request.Activity, activity);
+
+                await context.SaveChangesAsync(cancellationToken);
+            }
+        }
+    }
+}
---
+++ Application/Activities/Queries/GetActivityList.cs
@@ -0,0 +1,22 @@
+﻿using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Activities.Queries
+{
+    public class GetActivityList
+    {
+        public class Query: IRequest<List<Activity>> {}
+        public class Handler(AppDbContext context) : IRequestHandler<Query, List<Activity>>
+        {
+            public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                return await context.Activities.ToListAsync(cancellationToken);
+            }
+        }
+    }
+}
