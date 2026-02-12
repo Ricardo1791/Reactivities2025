3d22c152c6d1840aa8d788bedc66ca79dc1525cd
Ricardo <ric.alv_17@hotmail.com>
2026-02-12 14:08:29 -0500

    End of section 17

--- client/src/lib/types/index.d.ts
+++ client/src/lib/types/index.d.ts
@@ -13,7 +13,8 @@ type Activity = {
     isGoing: boolean
     isHost: boolean
     hostId: string
-    hostDisplayName: string
+    hostDisplayName: string,
+    hostImageUrl: string
 }
 
 type Profile = {
@@ -21,6 +22,11 @@ type Profile = {
   displayName: string
   bio?: string
   imageUrl?: string
+}
+
+type Photo = {
+  id: string,
+  url: string
 }
 
 type User = {
